f062d9b0cc2487fa702d07c01e35be738de6e50a
Nick Wylynko <nick@wylynko.com>
2024-06-06 12:50:14 +0800

    Add some information

--- src/App.tsx
+++ src/App.tsx
@@ -11,66 +11,77 @@ export default function App() {
       <main className="flex flex-col md:flex-row gap-4 items-center justify-center w-full">
         <div className="p-4 flex flex-col gap-4 min-w-48">
           <a
-            className="text-center text-blue-300 underline:hover"
+            className="text-center text-blue-300 hover:underline"
             href="https://npmjs.com/package/jotai"
           >
             Jotai
-          </a>{" "}
+          </a>
           <JotaiCounter />
         </div>
         <div className="p-4 flex flex-col gap-4 min-w-48">
           <a
-            className="text-center text-blue-300 underline:hover"
+            className="text-center text-blue-300 hover:underline"
             href="https://npmjs.com/package/zustand"
           >
             Zustand
-          </a>{" "}
+          </a>
           <ZustandCounter />
         </div>
         <div className="p-4 flex flex-col gap-4 min-w-48">
           <a
-            className="text-center text-blue-300 underline:hover"
+            className="text-center text-blue-300 hover:underline"
             href="https://npmjs.com/package/valtio"
           >
             Valtio
-          </a>{" "}
+          </a>
           <ValtioCounter />
         </div>
         <div className="p-4 flex flex-col gap-4 min-w-48">
           <a
-            className="text-center text-blue-300 underline:hover"
+            className="text-center text-blue-300 hover:underline"
             href="https://react.dev/reference/react/useState"
           >
             Use State
-          </a>{" "}
+          </a>
           <UseStateCounter />
         </div>
         <div className="p-4 flex flex-col gap-4 min-w-48">
           <a
-            className="text-center text-blue-300 underline:hover"
+            className="text-center text-blue-300 hover:underline"
             href="https://react.dev/reference/react/useContext"
           >
             Use Context
-          </a>{" "}
+          </a>
           <UseContextCounter />
         </div>
         <div className="p-4 flex flex-col gap-4 min-w-48">
           <a
-            className="text-center text-blue-300 underline:hover"
+            className="text-center text-blue-300 hover:underline"
             href="https://tkdodo.eu/blog/zustand-and-react-context"
           >
             Zustand With Context
-          </a>{" "}
+          </a>
           <ZustandWContextCounter />
         </div>
       </main>
       <footer className="flex flex-col items-center justify-center w-full">
         <a
-          className="text-center text-blue-300 underline:hover"
+          className="text-center text-blue-300 hover:underline"
           href="https://github.com/NWylynko/jotai-vs-zustand-vs-valtio"
         >
           Repo here
         </a>
+        <span className="max-w-[60ch]">
+          I recommend using the{" "}
+          <a
+            href="https://react.dev/learn/react-developer-tools"
+            className="text-center text-blue-300 hover:underline"
+          >
+            react dev tools
+          </a>{" "}
+          and enabling "Highlight updates when components render." to see which
+          implementations re-render what on state change.
+        </span>
       </footer>
     </div>
   );
